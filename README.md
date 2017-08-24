12f6e033f57478f4643f9986cfe9aa135cedbb55
devmassive <hosono-kyohei@kayac.com>
2017-08-24 19:01:22 +0900

    viable script

---
+++ README.md
@@ -0,0 +1,59 @@
+# appstore-googleplay-cvr-exporter
+
+Generates a CSV file that reports the CVR of your iOS / Android app.
+
+> CVR: Store Listing Visitor to Installer conversion rate
+
+You can easily import output files into Google Spreadhsheets or Excel.
+
+## Output CSV Columns
+
+- Date
+- Visitors (Android + iOS)
+- Installers (Android + iOS)
+- CVR (Android + iOS)
+- Android Visitors
+- Android Installers
+- Android CVR
+- iOS Visitors
+- iOS Installers
+- iOS CVR
+
+# Installation
+
+1. Set up `node.js`
+1. Set up `gsutils`
+
+
+## Install Dependencies
+
+> npm install
+
+## Create Config File
+
+`config/default.json`
+
+```js
+{
+    "ios" : {
+        "username" : "myname@example.com",      // iTunes Connect
+        "password" : "********",                // iTunes Connect
+        "appId"    : "00000000"                 // Found in My Apps -> App -> Apple ID or read below on getting the app id.
+    },
+    "android" : {
+        "packageName" : "com.example.myapp",    // The package name of your app
+        "bucketId"    : "pubsite_prod_rev_0000000000000000000" // Your Bucket ID of Google Cloud Storage
+    }
+}
+```
+
+# Usage
+
+> node . YYYY-MM
+
+`YYYY-MM`: year and month (default is this month)
+
+## Example
+
+> % node . 2017-08
+> % cat report/201708.csv
---
+++ index.js
@@ -0,0 +1,128 @@
+var moment = require('moment');
+var execSync = require('child_process').execSync;
+var fs = require('fs');
+var parse = require('csv-parse');
+
+var outData = [];
+var inDateLabel = process.argv[2];
+var inMoment = inDateLabel ? moment(inDateLabel, "YYYY-MM") : moment();
+var monthLabel  = inMoment.format("YYYYMM");
+console.log(inDateLabel + " -> " + monthLabel);
+var outFileName = 'csv/' + monthLabel + '.csv';
+
+var config = require('config');
+var bucketId = config.android.bucketId;
+var packageName = config.android.packageName;
+var username = config.ios.username;
+var password = config.ios.password;
+var appId = config.ios.appId;
+
+// Android
+var dstFileName = monthLabel + '.csv';
+
+var command = 'gsutil cp '
+    + 'gs://' + bucketId + '/acquisition/retained_installers/'
+    + 'retained_installers_' + packageName + '_'
+    + monthLabel
+    + '_country.csv '
+    + dstFileName;
+
+execSync(command, function(err, stdout, stderr) {});
+
+var parser = parse({columns: true});
+var readableStream = fs.createReadStream(dstFileName, {encoding: 'ucs2'});
+readableStream.pipe(parser);
+
+parser.on('readable', () => {
+  var data = parser.read();
+  if (data != null && data.Country == 'JP') {
+      var dateLabel = data.Date;
+      var visitors = data['Store Listing Visitors'];
+      var installers = data.Installers;
+      outData.push({
+          date : dateLabel,
+          android_visitors : +visitors,
+          android_installers : +installers,
+          android_cvr : 1.0 * installers / visitors
+      });
+  }
+});
+
+parser.on('end', () => {
+    fs.unlinkSync(dstFileName);
+});
+
+
+// iOS
+var itc = require('itunesconnectanalytics');
+var Itunes = itc.Itunes;
+var AnalyticsQuery = itc.AnalyticsQuery;
+
+var beginDateLabel  = inMoment.format("YYYY-MM") + '-01';
+var endDateLabel  = inMoment.format("YYYY-MM") + '-31';
+
+var instance = new Itunes(username, password, {
+    errorCallback: function(e) {
+        console.log('Error logging in: ' + e);
+    },
+    successCallback: function(d) {
+        console.log('Logged in');
+    }
+});
+
+var query = AnalyticsQuery.metrics(appId, {
+    measures: [itc.measures.pageViewUnique, itc.measures.units],
+    // 日本のみ
+    dimensionFilters: [
+		{dimensionKey: itc.dimensionFilterKey.territory, optionKeys: [143462]}
+	],
+}).date(beginDateLabel, endDateLabel);
+
+instance.request(query, function(error, result) {
+    for (var i = 0; i < result.results[0].data.length; i++) {
+        var pageViewRow = result.results[0].data[i];
+        var unitRow = result.results[1].data[i];
+        var dateLabel = pageViewRow.date.substring(0, 10);
+        outData.some(function(data) {
+            if (data.date == dateLabel) {
+                data.ios_visiters = +pageViewRow.pageViewUnique;
+                data.ios_installers = +unitRow.units;
+                data.ios_cvr = 1.0 * unitRow.units / pageViewRow.pageViewUnique;
+                return true;
+            }
+            return false;
+        });
+    }
+
+    var contentRows = [];
+    contentRows.push([
+        'Date',
+        'Visitors',
+        'Installers',
+        'CVR',
+        'Android Visitors',
+        'Android Installers',
+        'Android CVR',
+        'iOS Visitors',
+        'iOS Installers',
+        'iOS CVR'
+    ].join(','));
+    outData.forEach(function(data) {
+        contentRows.push([
+            data.date,
+            data.android_visitors + data.ios_visiters,
+            data.android_installers + data.ios_installers,
+            1.0 * (data.android_installers + data.ios_installers)
+                / (data.android_visitors + data.ios_visiters),
+            data.android_visitors,
+            data.android_installers,
+            data.android_cvr,
+            data.ios_visiters,
+            data.ios_installers,
+            data.ios_cvr
+        ].join(','));
+    });
+    fs.writeFile(outFileName, contentRows.join("\n"), 'utf8', function (err) {
+        console.log(err);
+    });
+});
